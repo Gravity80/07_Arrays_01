09000494de1004b1462edec9408dfcc189aa0278
Matthias_vdM <matthias.7@web.de>
2022-07-13 10:38:39 +0200

    getSentence() / Problem-Set

--- script_01.js
+++ script_01.js
@@ -8,28 +8,27 @@
 
 /*** 01. Funktionalität mit Einzelparametern */
 // --> "Ich bin Max Mütze."
- output(getSentence("Ich","bin","Max","Mütze"));
+// output(getSentence("Ich","bin","Max","Mütze"));
 // output(getSentence("Ich","bin","Maxine","Mützerich"));
-
-function getSentence(word1,word2,word3,word4) {
+// output(getSentence("Ich","bin","der", "coole","Max","Mütze"));
+// more of the same!
+function getSentence(word1,word2,word3,word4,word5,word6) {
 
     const gap   = " ";
     const punct = ".";
     let str   =     word1 + gap +
                     word2 + gap +
                     word3 + gap +
-                    word4 + 
+                    word4 + gap +
+                    word5 + gap +
+                    word6 +
                     punct;
 
     return str;
 }
-
-
 
 // Modul: Ausgabe in Konsole : Test
 // output("hi"); 
 function output(inputStr) {
     console.log(inputStr);
 }
-
-
